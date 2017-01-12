e594c4053aafd449e40140e0761cf0e356960ba6
Deepal <dsilvadeepal@gmail.com>
2017-01-11 21:53:48 -0800

    Update Up-Going-ch1-practice.js
    Add the JS code for first exercise - store purchase

--- Books/YDKJS/Up-Going-ch1-practice.js
+++ Books/YDKJS/Up-Going-ch1-practice.js
@@ -1,1 +1,36 @@
-## Store purchase
+const TAX_RATE = 0.07;
+const PHONE_PRICE = 300;
+const SPENDING_THRESHOLD = 450;
+const ACCESSORY_PRICE = 15;
+
+var bank_bal = 500;
+var amount = 0;
+
+function calcTax(amount){
+    return amount * TAX_RATE;
+}
+
+function formatAmount(amount) {
+    return "$" + amount.toFixed(2);
+}
+
+
+//keep buying phones while you have money in bank account
+while(amount < bank_bal) {
+    amount += PHONE_PRICE;
+    
+    //can we afford an accessory
+    if(amount < SPENDING_THRESHOLD){
+        amount += ACCESSORY_PRICE;
+    }
+}
+
+//calculate tax
+amount = amount + calcTax(amount);
+console.log("Your purchase : " + formatAmount(amount));
+
+
+//can you afford this purchase
+if(amount > bank_bal) {
+    console.log("You cannot afford this purchase!");
+}
